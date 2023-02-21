42a6f8ca7ae6f60e2129f2425dbe1353f8bd43ce
prince-kwakye <kwakyeoforiprince@gmail.com>
2023-02-21 12:55:56 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - How to print a line with printf function
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
