ce6bdbd6c823bcbb873efbb0d6647a59e37e4891
prince-kwakye <kwakyeoforiprince@gmail.com>
2023-02-22 14:12:08 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	char lower;
+	for (lower = 'z'; lower >= 'a'; lower--)
+		putchar(lower);
+	putchar('\n');
+	return (0);
+}
