5357e439b980e1807cfa01ca322b59b403a99d61
prince-kwakye <kwakyeoforiprince@gmail.com>
2023-02-24 03:41:48 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,10 +2,11 @@
 
 /**
  * main - Checks whether a given character is lowercase
+ * Parameters: alpha The character to check
  * Return: Returns  1 if the character is lowercase, 0 otherwise
  */
 
-int _islower(int alpha)
+int _islower(int alpha) /*function implementation*/
 {
 	if (alpha >= 'a' && alpha <= 'z')
 		return (1);
